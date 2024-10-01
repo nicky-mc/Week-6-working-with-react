277932dbab6dc1b290e2eda865d44c58fe9a4354
nicky-mc <cinimod@gmail.com>
2024-10-01 15:35:34 +0100

    modular css applied

--- CSS-IN-REACT/react-and-css/src/components/Navbar.jsx
+++ CSS-IN-REACT/react-and-css/src/components/Navbar.jsx
@@ -1,14 +1,29 @@
-export default function Navbar() {
+import "./Navbar.css"; // Make sure to create this CSS file
+
+function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar__logo">
-        <img src="/images/airbnb-logo.png" alt="Airbnb logo" />
-      </div>
-      <div className="navbar__links">
-        <a href="#top">Home</a>
-        <a href="#top">/About</a>
-        <a href="#tickets">/Tickets</a>
-      </div>
+      <img
+        src="https://images.pexels.com/photos/87651/earth-blue-planet-globe-planet-87651.jpeg"
+        alt="Logo"
+        className="navbar-logo"
+      />
+      <ul className="nav-links">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
     </nav>
   );
 }
+
+export default Navbar;
--- CSS-IN-REACT/react-and-css/src/components/Section.jsx
+++ CSS-IN-REACT/react-and-css/src/components/Section.jsx
@@ -1,11 +1,24 @@
 export default function Section(props) {
   return (
-    <div className="section">
-      <h1>{props.mainTitle}</h1> <br />
-      <p>{props.subTitle}</p> <br />
-      <p>{props.text}</p> <br />
-      <button>{props.buttonText}</button> <br />
-      <img src={props.img} alt={props.imgAlt} />
+    <div id={props.divid} className="section">
+      <div className="section-content">
+        <h2 id={props.h2id} className={`subtitle${props.number}`}>
+          {props.subtitle}
+        </h2>
+        <h1 id={props.h1id} className={`mainTitle${props.number}`}>
+          {props.mainTitle}
+        </h1>
+        <button id={props.buttonid}>{props.button}</button>
+      </div>
+      <div className="section-image-container">
+        <img
+          src={props.src}
+          alt={props.mainTitle}
+          className="section-image"
+          width={props.width}
+          height={props.height}
+        />
+      </div>
     </div>
   );
 }
